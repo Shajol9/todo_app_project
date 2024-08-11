dce5db421bb629fc95e0fce3dce1108393fe4a44
Mohammad Shazzad Hossain <89863207+Shajol9@users.noreply.github.com>
2024-08-11 11:05:17 +0200

    default parameter and doctext added to the functions

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ todo_file_path = os.path.join(script_dir,'todo.txt')
 
 
 def get_todo(filepath = todo_file_path):    # use of default parameter in a function defination
+    """
+    Reads a text file and returens it's 
+    content line by line as list of items.
+    """
     with open(filepath, 'r') as local_file:
         todos = local_file.readlines()
     return todos
 
 
 def write_doto(filepath, todo_list,):
+    """ 
+    Writes a list to a text file line by line.
+    """
     with open(filepath, 'w') as local_file:
         local_file.writelines(todo_list)
 
